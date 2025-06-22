ac8138eae0f092c7a01b97125a2e7532066b8c42
Robe <roberto.granda@thecodelight.com>
2025-06-22 15:46:49 -0300

    feat: add lang toggle component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterOutlet} from '@angular/router';
 
 import {NavItemComponent} from "./shared/nav-item/nav-item.component";
 import {ShootingStarComponent} from "./shared/shooting-star/shooting-star.component";
+import {LangToggleComponent} from "./shared/lang-toggle/lang-toggle.component";
 
 @Component({
-    selector: 'app-root',
-    imports: [RouterOutlet, NavItemComponent, ShootingStarComponent],
-    templateUrl: './app.component.html',
-    styleUrl: './app.component.css'
+  selector: 'app-root',
+  imports: [RouterOutlet, NavItemComponent, ShootingStarComponent, LangToggleComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
 
---
+++ src/app/shared/lang-toggle/lang-toggle.component.ts
@@ -0,0 +1,44 @@
+import {Component, inject, OnDestroy, OnInit, signal} from '@angular/core';
+import {NavigationEnd, Router} from "@angular/router";
+import {filter, Subject, takeUntil} from "rxjs";
+
+@Component({
+  selector: 'app-lang-toggle',
+  imports: [],
+  templateUrl: './lang-toggle.component.html',
+  styleUrl: './lang-toggle.component.css'
+})
+export class LangToggleComponent implements OnInit, OnDestroy {
+  public href = signal<string>('');
+
+  private router = inject(Router);
+
+  private destroy$ = new Subject<void>();
+
+  public ngOnInit(): void {
+    this.router.events
+      .pipe(
+        filter(event => event instanceof NavigationEnd),
+        takeUntil(this.destroy$)
+      )
+      .subscribe((event: NavigationEnd) => {
+        const path = event.urlAfterRedirects || event.url;
+        const lang = path.split('/')[1];
+
+        if (lang === 'en') {
+          return this.href.set(path.replace('/en/', '/es/'));
+        }
+
+        if (lang === 'es') {
+          this.href.set(path.replace('/es/', '/en/'));
+        }
+
+        this.href.set('/es' + path);
+      });
+  }
+
+  public ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+}
