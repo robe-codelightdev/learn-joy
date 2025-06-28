f0ee5152886863a13b591028e8d754851ee61177
Robe <roberto.granda@thecodelight.com>
2025-06-28 19:51:28 -0300

    feat: add star sparkle

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,10 +4,11 @@ import {RouterOutlet} from '@angular/router';
 import {NavItemComponent} from "./shared/nav-item/nav-item.component";
 import {ShootingStarComponent} from "./shared/shooting-star/shooting-star.component";
 import {LangToggleComponent} from "./shared/lang-toggle/lang-toggle.component";
+import {StarSparkleComponent} from "./shared/star-sparkle/star-sparkle.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, NavItemComponent, ShootingStarComponent, LangToggleComponent],
+  imports: [RouterOutlet, NavItemComponent, ShootingStarComponent, LangToggleComponent, StarSparkleComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/shared/star-sparkle/star-sparkle.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-star-sparkle',
+  imports: [],
+  templateUrl: './star-sparkle.component.html',
+  styleUrl: './star-sparkle.component.css'
+})
+export class StarSparkleComponent {
+
+}
